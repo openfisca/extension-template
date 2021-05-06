ef9ec4bad2654865ca3d6f35476dba93bc97591b
sandcha <sandcha@users.noreply.github.com>
2021-05-06 23:45:56 +0100

    Bump version number to 1.3.9

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name = "OpenFisca-Extension-Template",
-    version = "1.3.8",
+    version = "1.3.9",
     author = "OpenFisca Team",
     author_email = "contact@openfisca.org",
     classifiers = [
