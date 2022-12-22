1f3971198508da48838e3c03021f5c81acbe6b48
Mauko Quiroga <public@mauko.me>
2022-12-22 17:27:17 +0100

    Fix linter

--- openfisca_extension_template/tests/test_api.py
+++ openfisca_extension_template/tests/test_api.py
@@ -11,34 +11,43 @@ import pytest
 
 @pytest.fixture
 def host():
+    """Return the host to use for the API."""
+
     return "127.0.0.1"
 
 
 @pytest.fixture
 def port():
+    """Return a random port to use for the API."""
+
     return random.randint(5000, 5999)
 
 
 @pytest.fixture
 def endpoint():
+    """Return the endpoint to use for the API test."""
+
     return "spec"
 
 
 @pytest.fixture
 def payload(host, port, endpoint):
-    return {
-        "url": f"http://{host}:{port}/{endpoint}",
-        "timeout": 1,
-        }
+    """Return the payload to use for the API test."""
+
+    return {"url": f"http://{host}:{port}/{endpoint}", "timeout": 1}
 
 
 @pytest.fixture
 def pipe():
+    """Return a descriptor to use for the server subprocess."""
+
     return subprocess.PIPE
 
 
 @pytest.fixture
 def server(host, port, pipe):
+    """Return a server subprocess."""
+
     cmd = [
         "openfisca",
         "serve",
@@ -50,24 +59,25 @@ def server(host, port, pipe):
         str(port),
         ]
 
-    proc = subprocess.Popen(cmd, stdout = pipe, stderr = pipe)
+    with subprocess.Popen(cmd, stdout = pipe, stderr = pipe) as proc:
+        try:
+            _, out = proc.communicate(timeout = 1)
 
-    try:
-        _, out = proc.communicate(timeout = 1)
+        except TimeoutExpired as error:
+            out = error.stderr
 
-    except TimeoutExpired as error:
-        out = error.stderr
+        if f"Listening at: http://{host}:{port} ({proc.pid})" in str(out):
+            yield
+            proc.terminate()
 
-    if f"Listening at: http://{host}:{port} ({proc.pid})" in str(out):
-        yield
-        proc.terminate()
-
-    else:
-        proc.terminate()
-        raise SubprocessError(f"Server did not start!\n{out.decode('utf-8')}")
+        else:
+            proc.terminate()
+            raise SubprocessError(f"Failed to start!\n{out.decode('utf-8')}")
 
 
 def test_openfisca_server(server, payload):
+    """Test the OpenFisca API serves the /spec endpoint."""
+
     with request.urlopen(**payload) as response:
         data = json.loads(response.read().decode("utf-8"))
         assert data["info"]["title"] == "Openfisca-Country-Template Web API"
