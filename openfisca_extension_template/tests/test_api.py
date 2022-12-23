29ea784c5a5987e6642a9bdfe6535e68280f88e4
Mauko Quiroga <public@mauko.me>
2022-12-23 10:59:32 +0100

    Simplify smoke test

--- openfisca_extension_template/tests/test_api.py
+++ openfisca_extension_template/tests/test_api.py
@@ -3,49 +3,29 @@
 from subprocess import SubprocessError, TimeoutExpired
 from urllib import request
 import json
-import random
 import subprocess
 
 import pytest
 
-
-@pytest.fixture
-def host():
-    """Return the host to use for the API."""
-
-    return "127.0.0.1"
-
-
-@pytest.fixture
-def port():
-    """Return a random port to use for the API."""
-
-    return random.randint(5000, 5999)
+endpoint = "spec"
+host = "127.0.0.1"
+pipe = subprocess.PIPE
+port = 5000
+timeout = 10
 
 
 @pytest.fixture
-def endpoint():
-    """Return the endpoint to use for the API test."""
-
-    return "spec"
-
-
-@pytest.fixture
-def payload(host, port, endpoint):
+def payload():
     """Return the payload to use for the API test."""
 
-    return {"url": f"http://{host}:{port}/{endpoint}", "timeout": 1}
+    return {
+        "timeout": timeout,
+        "url": f"http://{host}:{port}/{endpoint}",
+        }
 
 
 @pytest.fixture
-def pipe():
-    """Return a descriptor to use for the server subprocess."""
-
-    return subprocess.PIPE
-
-
-@pytest.fixture
-def server(host, port, pipe):
+def _server():
     """Return a server subprocess."""
 
     cmd = [
@@ -60,24 +40,33 @@ def server(host, port, pipe):
         ]
 
     with subprocess.Popen(cmd, stdout = pipe, stderr = pipe) as proc:
-        try:
-            _, out = proc.communicate(timeout = 1)
+        for _ in range(timeout * 100 + 1):
+            try:
+                _, out = proc.communicate(timeout = timeout / 100)
 
-        except TimeoutExpired as error:
-            out = error.stderr
+            except TimeoutExpired as error:
+                out = error.stderr
+
+            if out is not None:
+                break
 
         if f"Listening at: http://{host}:{port} ({proc.pid})" in str(out):
             yield
             proc.terminate()
 
-        else:
+        elif out is not None:
             proc.terminate()
             raise SubprocessError(f"Failed to start!\n{out.decode('utf-8')}")
 
+        else:
+            proc.terminate()
+            raise SubprocessError("Failed to start!")
 
-def test_openfisca_server(server, payload):
+
+def test_openfisca_server(payload, _server):
     """Test the OpenFisca API serves the /spec endpoint."""
 
     with request.urlopen(**payload) as response:
         data = json.loads(response.read().decode("utf-8"))
+        assert response.status == 200
         assert data["info"]["title"] == "Openfisca-Country-Template Web API"
