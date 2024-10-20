bcca234fb256bcacb150325e2780349d7d3425db
Mauko Quiroga <public@mauko.me>
2024-10-20 15:31:04 +0200

    build: add bashdep

---
+++ bashdep.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Ensure bashdep is installed
+[[ ! -f lib/bashdep ]] && {
+  mkdir -p lib
+  curl -sLo lib/bashdep \
+    https://github.com/Chemaclass/bashdep/releases/download/0.1/bashdep
+  chmod +x lib/bashdep
+}
+
+# Add latest bashunit release to your dependencies
+readonly DEPENDENCIES=(
+  "https://github.com/TypedDevs/bashunit/releases/download/0.18.0/bashunit"
+)
+
+# Load, configure and run bashdep
+source lib/bashdep
+bashdep::setup dir="lib" silent=false
+bashdep::install "${DEPENDENCIES[@]}"
